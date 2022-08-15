d8c3355e2f46a8e96aac2094f58afdef526fa61c
q5nguyenn <q5nguyenn@gmail.com>
2022-08-15 11:15:05 +0700

    Update

--- js/style.js
+++ js/style.js
@@ -95,3 +95,24 @@ function isEmail(emailStr)
             image.src = `images/home${index}.jpg`
         }
     })
+
+// Phần mobile
+var box = document.querySelector('.boxbox');
+var btnMobile = document.querySelector('.header-right-mobile');
+var menuMobile = document.querySelector('.mobile-container');
+// var headerMobile = document.querySelector('.header-mobile');
+var xClose = document.querySelector('.mobile-container a:last-child');
+
+xClose.onclick = function (){
+    menuMobile.classList.remove('show');
+};
+box.onclick = function (){
+    menuMobile.classList.remove('show');
+};
+menuMobile.onclick = function (e) {
+    e.stopPropagation();
+};
+btnMobile.onclick = function (e){
+    e.stopPropagation();
+    menuMobile.classList.add('show');
+};
